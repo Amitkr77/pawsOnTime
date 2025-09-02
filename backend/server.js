338d962f955209ebdf9048b42d7113ebbde6f4c7
Amitkr77 <amitroyk99@gmail.com>
2025-09-02 21:06:10 +0530

    added get api route

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,10 @@ app.use('/api/walker', walkerRoutes);
 app.use("/api/users", userRoutes);
 
 
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
