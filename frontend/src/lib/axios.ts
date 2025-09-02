4d96e4cc13dc7a320bc9dc3f9dcefe669d7b232f
Amitkr77 <amitroyk99@gmail.com>
2025-09-02 21:25:08 +0530

    added backend ur;

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ Update this to your backend base URL
+  baseURL: "https://pawsontime.onrender.com/api", 
   headers: {
     "Content-Type": "application/json"
   }
